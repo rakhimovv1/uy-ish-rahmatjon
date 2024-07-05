01608474254aa7da83eec974d1312bfab2e42012
rakhimovv1 <rakhimov225522@gmail.com>
2024-07-05 16:56:58 +0200

    Add files via upload

---
+++ figma1-usmon-main/index.js
@@ -0,0 +1,66 @@
+let h1 = document.getElementById("h1")
+
+h1.onclick = function() {
+  h1.style.backgroundColor = "yellowgreen"
+  h1.style.color = "#fff"
+
+}
+
+let p = document.getElementById("p")
+
+p.onclick = function () {
+    p.style.backgroundColor = 'green'
+    p.style.color = '#fff'
+}
+
+let h2 = document.getElementById("h2")
+
+h2.onclick = function() {
+  h2.style.backgroundColor = "yellow"
+  h2.style.color = "#fff"
+
+}
+
+let p2 = document.getElementById("p2")
+
+p2.onclick = function () {
+    p2.style.backgroundColor = 'orange'
+    p2.style.color = '#fff'
+}
+
+let div = document.getElementById("div")
+
+div.onclick = function () {
+    div.style.backgroundColor = 'black'
+    
+}
+
+
+let div2 = document.getElementById("div2")
+
+div2.onclick = function () {
+    div2.style.backgroundColor = 'orange'
+    
+}
+
+let span = document.getElementById("span")
+
+span.onclick = function () {
+    span.style.backgroundColor = 'blue'
+    
+}
+
+
+let icons = document.getElementById("icons")
+
+icons.onclick = function () {
+    icons.style.backgroundColor = 'blue'
+    
+}
+
+let nav = document.getElementById("nav")
+
+nav.onclick = function () {
+    nav.style.backgroundColor = 'green'
+    
+}
